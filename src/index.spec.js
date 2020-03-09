788033a091604138c38a5516e7dbf7157fca6a27
AJ Foster <web@aj-foster.com>
2020-03-09 00:57:55 -0400

    Add additional test shell for the main module

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,19 +1,25 @@
 /* eslint-disable no-unused-vars */
 const { describe, it } = require("mocha");
-const { lf } = require(global.indexFile || "index.js");
+const { lf } = global.lf || require("index.js");
 
 describe("CRLF Converter", () => {
   describe("lf", () => {
-    it("should replace CRLF at the end of a string", () => {
-      const result = lf`Hello there.\r\n`;
-
-      // ...
-    });
-
     it("should replace CRLF at the beginning of a string", () => {
       const result = lf`\r\nHello there.`;
 
-      // ...
+      // Use `String.prototype.startsWith()`
+    });
+
+    it("should replace CRLF at the end of a string", () => {
+      const result = lf`Hello there.\r\n`;
+
+      // Use `String.prototype.endsWith()`
+    });
+
+    it("should replace CRLF in the middle of a string", () => {
+      const result = lf`Hello \r\n\r\nthere.`;
+
+      // Use `String.prototype.includes()`
     });
   });
 });
