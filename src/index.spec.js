66e828b8baf016fe9b050a47b2ab287442a191b8
AJ Foster <web@aj-foster.com>
2020-03-09 00:59:10 -0400

    Add assertions to main module tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+const { assert } = require("chai");
 const { describe, it } = require("mocha");
 const { lf } = global.lf || require("index.js");
 
@@ -7,19 +8,19 @@ describe("CRLF Converter", () => {
     it("should replace CRLF at the beginning of a string", () => {
       const result = lf`\r\nHello there.`;
 
-      // Use `String.prototype.startsWith()`
+      assert(result.startsWith("\nHello"));
     });
 
     it("should replace CRLF at the end of a string", () => {
       const result = lf`Hello there.\r\n`;
 
-      // Use `String.prototype.endsWith()`
+      assert(result.endsWith("there.\n"));
     });
 
     it("should replace CRLF in the middle of a string", () => {
       const result = lf`Hello \r\n\r\nthere.`;
 
-      // Use `String.prototype.includes()`
+      assert(result.includes("o \n\nt"));
     });
   });
 });
